88e8591682ac7eea9fe673b4155e4187f1bbf019
raiguard <caleb.heuer@gmail.com>
2020-07-08 14:29:31 -0600

    Register user endpoint

--- server/index.js
+++ server/index.js
@@ -3,13 +3,21 @@ const massive = require("massive");
 const session = require("express-session");
 require("dotenv").config();
 
+const authController = require("./controllers/authController");
+
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
 
+// create app
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(session({ resave: true, saveUninitialized: false, secret: SESSION_SECRET }));
 
+// endpoints
+app.post("/auth/register", authController.register);
+
+// connect to database
 massive({
   connectionString: CONNECTION_STRING,
   ssl: { rejectUnauthorized: false }
@@ -18,4 +26,5 @@ massive({
   console.log("Database connection established");
 });
 
+// init server
 app.listen(SERVER_PORT, () => console.log(`Server running on port: ${SERVER_PORT}`));
